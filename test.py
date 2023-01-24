5db97a436e89dfc1da87e6e8406c32570b551463
SESA680579 <lokanath.panda@se.com>
2023-01-24 19:20:18 +0530

    new folder commit

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ if __name__=='__main__':
     np_array = np.array([1, 2, 3])
     print("Sum of Numpy Array: ",np.sum(np_array))
     df = pd.DataFrame(np_array)
-    print("Pandas Dataframe:  ", df)
+    print("Pandas Dataframe >>>>>>>>>>>>>>>>>:  ", df)
